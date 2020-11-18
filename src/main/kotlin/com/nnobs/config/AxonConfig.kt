e986ad0d31f8f64eb1d840a86b0e404b3d0ce732
logan <logan.81k@gmail.com>
2020-11-18 12:49:00 +0900

    HELP.md 정리

---
+++ src/main/kotlin/com/nnobs/config/AxonConfig.kt
@@ -0,0 +1,16 @@
+package com.nnobs.config
+
+import com.nnobs.domain.aggregate.UserAggregate
+import org.axonframework.eventsourcing.EventSourcingRepository
+import org.axonframework.eventsourcing.eventstore.EventStore
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+
+@Configuration
+class AxonConfig {
+    @Bean
+    fun accountAggregateEventSourcingRepository(eventStore: EventStore): EventSourcingRepository<UserAggregate> {
+        return EventSourcingRepository.builder(UserAggregate::class.java).eventStore(eventStore).build()
+    }
+}
